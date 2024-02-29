4282461146c283fdb259d2735fa39a51473afdd5
Jesus <jesus.rangel@cceo.com.mx>
2024-02-29 11:54:34 -0600

    Modificacion en estilo de textos, posicionamiento de widgets y estilo appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,13 +4,28 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget{
   const HomeScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+
+    TextStyle fontSize35 = const TextStyle(fontSize: 35);
+    TextStyle fontSize30 = const TextStyle(fontSize: 30);
+
+    return Scaffold(
+      appBar:AppBar(
+        backgroundColor: Colors.blueAccent,
+        elevation: 1,
+        title:const Text('HomeScreen'),
+        
+      ) ,
       backgroundColor: Colors.greenAccent,
-      body:Center(
-          child: Text('Home Screen'),
+      body:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                Text('Número de clicks', style: fontSize35),
+                Text('25', style: fontSize30)
+            ],
+          ),
       ),
     );
   }
