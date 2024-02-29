5eed12e581bec507036c37a4dcd7daa967fee1d7
Jesus <jesus.rangel@cceo.com.mx>
2024-02-29 11:06:55 -0600

    Creacion y diseño de HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
+import 'package:counter_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(
-        child: Text('Hola Mundo xd'),
-        )
+    return  const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen()
     );
   }
 
